a53bd0f7a2f02d3e4b9173586a71a8015a903330
eMAN-pl <eman.foto@gmail.com>
2026-02-04 01:01:32 +0100

    Disable minification to fix Kaboom build issue

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/shiji-quest/',
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
